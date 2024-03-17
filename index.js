732915ba7dc17703b58efea16921df844aaac4df
dfghjkcvbnmnbvcx <163096242+kbkhkj@users.noreply.github.com>
2024-03-17 13:40:54 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+function convertToUppercase(str) {
+    return str.toUpperCase();
+  }
+  
+  const lowercaseString = 'hello world';
+  const uppercaseString = convertToUppercase(lowercaseString);
+  console.log(`Uppercase string:`, uppercaseString);
+  
